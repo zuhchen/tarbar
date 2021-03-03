e689fc8289dca9ba78f46fd4b16ad24691bbcf55
zuhchen <18312160734@139.com>
2021-03-03 20:22:45 +0800

    App页面基本布局

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,28 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div id="tarbar">
+    <div class="tarbarItem">首页</div>
+    <div class="tarbarItem">分类</div>
+    <div class="tarbarItem">购物车</div>
+    <div class="tarbarItem">我的</div>
   </div>
-  <router-view/>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+@import "./assets/css/base.css";
+#tarbar {
+  position: fixed;
+  display: flex;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  flex-flow: row nowrap;
+  background-color: rgb(245, 242, 237);
+  justify-content: space-around;
+  box-shadow: 0 -3px 5px rgb(10, 10, 10);
+}
+
+.tarbarItem {
+  height: 49px;
   text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
 }
 </style>
