be090b01a921350359d2696214a8b12a51a62a07
zuhchen <18312160734@139.com>
2021-03-05 21:47:10 +0800

    v1.1.9,首页导航栏初始化

---
+++ src/components/common/navbar/navbar.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="navbar">
+        <div class="left">
+            <slot name="left"></slot>
+        </div>
+        <div class="center">
+            <slot name="center"></slot>
+        </div>
+        <div class="right">
+            <slot name="right"></slot>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'NavBar'
+}
+</script>
+
+<style scoped>
+    .navbar{
+        display: flex;
+        box-shadow: 0 1px 1px red;
+        height: 44px;
+        text-align: center;
+    }
+    .left,.right{
+        width:60px;
+    }
+    .center{
+        flex:1;
+    }
+
+</style>
--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -1,5 +1,22 @@
 <template>
     <div>
-        首页
+        <nav-bar>
+            <template v-slot:center>
+                <div>
+                    购物车
+                </div>
+            </template>
+        </nav-bar>
     </div>
 </template>
+
+<script>
+import NavBar from 'components/common/navbar/navbar'
+
+export default {
+    name:'Home',
+    components: {
+        NavBar
+    }
+}
+</script>
